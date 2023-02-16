f86cea05f23e80956e2f67ab5ccb76f4e8cf445e
KennedyB1 <114904314+KennedyB1@users.noreply.github.com>
2023-02-16 23:39:14 +0100

    Update g.ts

--- src/ts/g.ts
+++ src/ts/g.ts
@@ -5,15 +5,11 @@
   */
 
 function getLength(jumpings: number[]): number {
-  let totalNumber = 0;
-
-  totalNumber = jumpings.reduce(
-    (jumpDistanceSoFar, currentJump) => jumpDistanceSoFar + currentJump
+  return jumpings.reduce(
+    (jumpDistanceSoFar, currentJump) => { return jumpDistanceSoFar + currentJump }
   );
-
-  return totalNumber;
-}
-
+};
+////////////////////////////////////////////////////////////////////////
 /*
   2. I detta exempel har vi fokuserat på if-statements. Se om du kan göra exemplet bättre!
   */
@@ -23,23 +19,21 @@ class Student {
     public name: string,
     public handedInOnTime: boolean,
     public passed: boolean
-  ) {}
+  ) { }
 }
 
-function getStudentStatus(student: Student): string {
-  student.passed =
-    student.name == "Sebastian"
-      ? student.handedInOnTime
-        ? true
-        : false
-      : false;
-
-  if (student.passed) {
-    return "VG";
-  } else {
-    return "IG";
+function getStudentGrade(student: Student): string {
+  if (student.name === "Sebastian") {
+    if (student.handedInOnTime) {
+      student.passed = true;
+      return "VG";
+    }
   }
+  student.passed = false;
+  return "IG";
 }
+
+
 
 /*
   3. Variabelnamn är viktiga. Kika igenom följande kod och gör om och rätt.
@@ -47,21 +41,24 @@ function getStudentStatus(student: Student): string {
   */
 
 class Temp {
-  constructor(public q: string, public where: Date, public v: number) {}
+  constructor(public city: string, public time: Date, public topTemperature: number) { }
 }
 
 function averageWeeklyTemperature(heights: Temp[]) {
-  let r = 0;
+  let totalTemperature = 0;
+  let averageTemperature = 0;
+  const timeInMs = 604800000
+  const daysInAWeek = 7;
 
   for (let who = 0; who < heights.length; who++) {
-    if (heights[who].q === "Stockholm") {
-      if (heights[who].where.getTime() > Date.now() - 604800000) {
-        r += heights[who].v;
+    if (heights[who].city === "Stockholm") {
+      if (heights[who].time.getTime() > Date.now() - timeInMs) {
+        totalTemperature += heights[who].topTemperature;
       }
     }
   }
-
-  return r / 7;
+  averageTemperature = totalTemperature / daysInAWeek;
+  return
 }
 
 /*
@@ -69,33 +66,38 @@ function averageWeeklyTemperature(heights: Temp[]) {
   Se om du kan göra det bättre. Inte bara presentationen räknas, även strukturer.
   */
 
-function showProduct(
-  name: string,
-  price: number,
-  amount: number,
-  description: string,
-  image: string,
-  parent: HTMLElement
-) {
-  let container = document.createElement("div");
-  let title = document.createElement("h4");
-  let pris = document.createElement("strong");
-  let imageTag = document.createElement("img");
 
-  title.innerHTML = name;
-  pris.innerHTML = price.toString();
-  imageTag.src = image;
-
-  container.appendChild(title);
-  container.appendChild(imageTag);
-  container.appendChild(pris);
-  parent.appendChild(container);
+class Product {
+  constructor(
+    public name: string,
+    public price: number,
+    public amount: number,
+    public description: string,
+    public image: string
+  ) { }
 }
 
+function showProduct(product: Product, parent: HTMLElement) {
+  const container = document.createElement("div");
+
+  const title = document.createElement("h4");
+  title.innerHTML = `${product.name}`;
+  container.appendChild(title);
+
+  const price = document.createElement("strong");
+  price.innerHTML = `${product.price}`;
+  container.appendChild(price);
+
+  const imageTag = document.createElement("img");
+  imageTag.src = product.image;
+  container.appendChild(imageTag);
+
+  parent.appendChild(container);
+}
 /*
   5. Följande funktion kommer presentera studenter. Men det finns ett antal saker som 
   går att göra betydligt bättre. Gör om så många som du kan hitta!
-  */
+  
 function presentStudents(students: Student[]) {
   for (const student of students) {
     if (student.handedInOnTime) {
@@ -119,29 +121,22 @@ function presentStudents(students: Student[]) {
     }
   }
 }
-
+*/
 /*
-  6. Skriv en funktion som skall slå ihop följande texter på ett bra sätt:
-  Lorem, ipsum, dolor, sit, amet
-  Exemplet under löser problemet, men inte speciellt bra. Hur kan man göra istället?
+  6. Skriv en funktion som skall slå ihop följande texter på ett bra sätt: Lorem, ipsum, dolor, sit, amet Exemplet under löser problemet, men inte speciellt bra. Hur kan man göra istället?
   */
-function concatenateStrings() {
-  let result = "";
-  result += "Lorem";
-  result += "ipsum";
-  result += "dolor";
-  result += "sit";
-  result += "amet";
-
-  return result;
+function getLoremIpsum(): string {
+  const strings = ["Lorem", "ipsum", "dolor", "sit", "amet"];
+  return strings.join(" ");
 }
 
+////////////////////////////
 /* 
 7. Denna funktion skall kontrollera att en användare är över 20 år och göra någonting.
     Det finns dock problem med denna typ av funktion. Vad händer när kraven ändras och
     fler och fler parametrar behöver läggas till? T.ex. avatar eller adress. Hitta en bättre
     lösning som är hållbar och skalar bättre. 
-*/
+
 function createUser(
   name: string,
   birthday: Date,
@@ -162,3 +157,4 @@ function createUser(
     return "Du är under 20 år";
   }
 }
+*/
